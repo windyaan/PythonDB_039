185f1e816f7a31d3381276998656db665cb34e47
windyaan <windyanurmaretha@gmail.com>
2023-11-23 12:03:49 +0700

    kedua

--- PythonDB_039.py
+++ PythonDB_039.py
@@ -84,28 +84,6 @@ def klikButton ():
     
     messagebox.showinfo("HASIL PREDIKSI :", f'{HASIL}')
 
-# def simpan_data_ke_sqlite(NAMA,BIOLOGI, FISIKA, ING, HASIL_PREDIKSI):
-    # # Membuka atau membuat database SQLite
-    # conn = sqlite3.connect("data.db")
-    # cursor = conn.cursor()
-    
-    # # Membuat tabel jika belum ada
-    # cursor.execute('''CREATE TABLE IF NOT EXISTS hasil_prediksi
-    #                 (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    #                 NAMA TEXT,
-    #                 BIOLOGI INT, 
-    #                 FISIKA INT,
-    #                 ING INT,
-    #                 HASIL_PREDIKSI TEXT)''')
-    
-    # # Memasukkan data mata pelajaran ke dalam tabel
-    # cursor.execute("INSERT INTO NILAI (NAMA, BIOLOGI, FISIKA, ING, HASIL_PREDIKSI) VALUES (?, ?, ?, ?, ?)",
-    # (NAMA, BIOLOGI, FISIKA, ING, HASIL))
-    
-    # # Melakukan commit dan menutup koneksi
-    # conn.commit()
-    # conn.close()
-
 buttonSubmit = ttk.Button(inputFrame, text="PREDIKSI SEKARANG", command=klikButton)
 buttonSubmit.pack(padx=10, pady=10, fill="x", expand=True)
 
